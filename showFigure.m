2430a7e1f95829a253f6fdeec60cfe3f360546ac
zq <2285053637@qq.com>
2022-05-27 15:46:19 +0800

    change the name of some variables

--- showFigure.m
+++ showFigure.m
@@ -7,12 +7,12 @@ Torque_2 = M(:, end-2); % 提取2#通道扭矩
 clear M
 %% 提取扭矩峰值和谷值
 % 峰值提取
-plusTF = (Torque_2 > 0);
-plusTime = Time(plusTF); plusTorque_2 = Torque_2(plusTF);
+isPlus = (Torque_2 > 0);
+plusTime = Time(isPlus); plusTorque_2 = Torque_2(isPlus);
 [peaks, maxIdx] = findpeaks(plusTorque_2,'MinPeakProminence',max(Torque_2)/2);
 % 谷值提取
-minusTF = (Torque_2 < 0);
-minusTime = Time(minusTF); minusTorque_2 = Torque_2(minusTF);
+isMinus = (Torque_2 < 0);
+minusTime = Time(isMinus); minusTorque_2 = Torque_2(isMinus);
 [valleys, minIdx] = findpeaks(-minusTorque_2,'MinPeakProminence',-min(Torque_2)/2);
 %% 绘图
 subplot(1,2,1);
